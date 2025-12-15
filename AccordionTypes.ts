1fc96ae262a71684ed708fd12372e59b05bb9c22
misterbombay <87628148+misterbombay@users.noreply.github.com>
2025-12-15 20:27:04 +0530

    Create AccordionTypes.ts

---
+++ AccordionTypes.ts
@@ -0,0 +1,54 @@
+/**
+ * Accordion Header Props
+ */
+export interface IAccordionHeaderProps {
+  headerTitle: string;
+  showHeaderIcon: boolean;
+  headerIconUrl?: string;
+
+  iconPosition: 'left' | 'right';
+  togglePosition: 'left' | 'right';
+
+  headerBgColor: string;
+  headerTextColor: string;
+  headerFontSize: number;
+  headerFontFamily: string;
+  headerPadding: number;
+
+  headerBorder: boolean;
+  headerBorderColor: string;
+  headerBorderWidth: number;
+
+  headerRounded: boolean;
+
+  isOpen: boolean;
+  onToggle: () => void;
+}
+
+/**
+ * Accordion Body Props
+ */
+export interface IAccordionBodyProps {
+  bodyText: string;
+  bodyImageUrl?: string;
+
+  bodyBgColor: string;
+  bodyTextColor: string;
+  bodyFontSize: number;
+  bodyFontFamily: string;
+  bodyPadding: number;
+
+  bodyBorder: boolean;
+  bodyBorderColor: string;
+  bodyBorderWidth: number;
+
+  bodyRounded: boolean;
+}
+
+/**
+ * Combined Accordion Props
+ * Used by Accordion container component
+ */
+export interface IAccordionProps
+  extends Omit<IAccordionHeaderProps, 'isOpen' | 'onToggle'>,
+    IAccordionBodyProps {}
